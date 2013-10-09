c8b85716036e337939521e67d120f455c7a85707
MarkyC <mmmarcooo@gmail.com>
2013-10-09 19:26:45 -0400

    Fixed Warning.

--- ApplicationCenter/src/com/github/markyc/applicationcenter/DisplayPanel.java
+++ ApplicationCenter/src/com/github/markyc/applicationcenter/DisplayPanel.java
@@ -4,8 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.List;
 
 import javax.swing.JPanel;
